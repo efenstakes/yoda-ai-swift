ad1be2f3bfe4121696b75dceac9e83fa5465cea5
Efen Stakes <efenstakes101@gmail.com>
2023-10-17 13:56:13 +0300

    Initial Commit

---
+++ YodaAI/YodaAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YodaAIApp.swift
+//  YodaAI
+//
+//  Created by felix ids on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct YodaAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
