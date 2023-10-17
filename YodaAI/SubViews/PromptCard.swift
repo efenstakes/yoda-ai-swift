956e0abbef3d295721f94e64cc436813db92a2d8
Efen Stakes <efenstakes101@gmail.com>
2023-10-17 14:25:07 +0300

    add prompt card

--- YodaAI/SubViews/PromptCard.swift
+++ YodaAI/SubViews/PromptCard.swift
@@ -8,11 +8,90 @@
 import SwiftUI
 
 struct PromptCard: View {
+    var prompt: Prompt
+    
+    @State var showCopiedSuccessfully: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+
+            // prompt
+            HStack(alignment: .top) {
+                VStack{
+                    Text(prompt.prompt)
+                }
+                .padding()
+                .frame(width: UIScreen.main.bounds.width * 0.85, alignment: .leading)
+                .background(Color(hex: "F2E8DF"))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            .frame(width: UIScreen.main.bounds.width - 20, alignment: .leading)
+            
+            // reply
+            VStack {
+                VStack(alignment: .leading) {
+                    
+                    HStack {
+                        
+                        Spacer()
+                        
+                        if !showCopiedSuccessfully {
+                            
+                            Image(systemName: "clipboard")
+                                .resizable()
+                                .renderingMode(.original)
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 12)
+                                .foregroundColor(.white)
+                                .onTapGesture {
+                                    print("copy reply \(prompt.reply)")
+
+                                    let pasteboard = UIPasteboard.general
+                                    pasteboard.string = prompt.reply
+                                    
+                                    withAnimation {
+                                        showCopiedSuccessfully.toggle()
+                                    }
+                                    
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                                        
+                                        withAnimation {
+                                            showCopiedSuccessfully.toggle()
+                                        }
+                                    }
+
+                                }
+                            
+                        }
+                        
+                        if showCopiedSuccessfully {
+                            
+                            Text("Copied")
+                                .font(.footnote)
+                                .foregroundColor(.white)
+                            
+                        }
+                        
+                    }
+                    
+                    Text(prompt.reply)
+                        .foregroundColor(.white)
+                }
+                .padding()
+                .frame(width: UIScreen.main.bounds.width * 0.85, alignment: .leading)
+                // #182625, 486966
+                .background(Color(hex: "486966"))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            .frame(width: UIScreen.main.bounds.width - 20, alignment: .trailing)
+            
+            
+        }
     }
 }
 
 #Preview {
-    PromptCard()
+    PromptCard(
+        prompt: prompts[0]
+    )
 }
