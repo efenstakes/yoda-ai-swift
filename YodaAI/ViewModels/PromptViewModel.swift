9fca561a55a3b6ab22d3bd1b61bad390cc62e5c5
Efen Stakes <efenstakes101@gmail.com>
2023-10-17 14:12:28 +0300

    add prompt view model to manage user prompts data

--- YodaAI/ViewModels/PromptViewModel.swift
+++ YodaAI/ViewModels/PromptViewModel.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+enum PromptError {
+    case emptyPrompt
+    case networkError
+}
+
+@MainActor
+class PromptViewModel: ObservableObject {
+    @Published var isLoading: Bool = false
+    @Published var error: PromptError? = nil
+    @Published var prompts: [Prompt] = []
+    
+    
+    // this function receives the users prompt as a parameter and queries yoda ai for a reply
+    func getPrompt(for prompt: String) async {
+        guard !prompt.isEmpty else { return }
+        isLoading = true
+        error = nil
+        
+        // finally, set isloading to false
+        defer {
+            isLoading = false
+        }
+        
+        print("going to search now")
+        do {
+            let prompt = try await PromptService.prompt(prompt: prompt)
+            
+            // The post request was successful.
+            print("got response ")
+            print(prompt.prompt)
+            print(prompt.reply)
+            
+            prompts.append(prompt)
+        } catch {
+            // The post request failed.
+            print(error)
+            
+            self.error = PromptError.networkError
+        }
+    }
+    
+}
